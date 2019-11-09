842d1d0b0983b368e0488f573a9391028394a0ea
aarshic <aarshi.1998@gmail.com>
2019-11-09 13:30:28 +0530

    Side Menu

--- patient/app.js
+++ patient/app.js
@@ -6,11 +6,11 @@ var MongoStore = require('connect-mongo')(session);
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/testForAuth');
+mongoose.connect('mongodb://localhost/emaid');
 var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {});
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {});
 
 //use sessions for tracking logins
 app.use(session({
@@ -22,32 +22,32 @@ app.use(session({
   })
 }));
 
-// parse incoming requests
+//parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// serve static files from template
+//serve static files from template
 app.use(express.static(__dirname + '/template'));
 
-// include routes
+//include routes
 var routes=require('./routes/router');
 app.use('/', routes);
 
-// catch 404 and forward to error handler
+//catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('File Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
-// define as the last app.use callback
+//error handler
+//define as the last app.use callback
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.send(err.message);
 });
 
-// listen on port 3000
+//listen on port 3000
 app.listen(3000, function () {
-  console.log('Express app listening on port 3000');
+  console.log('Server running on port 3000');
 });
--- patient/routes/router.js
+++ patient/routes/router.js
@@ -4,7 +4,7 @@ var User = require('../models/user');
 
 // GET route for reading data
 router.get('/', function (req, res, next) {
-  return res.sendFile(path.join(__dirname + '/templateLogReg/index.html'));
+  return res.sendFile(path.join(__dirname + '/template/signup.html'));
 });
 
 //POST route for updating data
